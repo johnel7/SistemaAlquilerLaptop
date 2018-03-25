dd2a00039c0c428cf8f7a85c9feb05adf40774cb
Nelson Rojas Gonzales <37041001+johnel7@users.noreply.github.com>
2018-03-24 22:12:43 -0500

    se implemento el framework codeIgniter y se conecto con la base de datos (dbalquiler)

---
+++ application/views/layouts/aside.php
@@ -0,0 +1,49 @@
+        <!-- =============================================== -->
+
+        <!-- Left side column. contains the sidebar -->
+        <aside class="main-sidebar">
+            <!-- sidebar: style can be found in sidebar.less -->
+            <section class="sidebar">      
+                <!-- sidebar menu: : style can be found in sidebar.less -->
+                <ul class="sidebar-menu" data-widget="tree">
+                    <li class="header">MENU NAVEGACION</li>
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-home"></i> <span>Inicio</span>
+                        </a>
+                    </li>
+                    <li class="treeview">
+                        <a href="#">
+                            <i class="fa fa-cogs"></i> <span>Mantenimiento</span>
+                            <span class="pull-right-container">
+                                <i class="fa fa-angle-left pull-right"></i>
+                            </span>
+                        </a>
+                        <ul class="treeview-menu">
+                            <li><a href="<?php echo base_url();?>mantenimiento/choferes"><i class="fa fa-circle-o"></i> Choferes</a></li>
+                            <li><a href="<?php echo base_url();?>mantenimiento/vehiculos"><i class="fa fa-circle-o"></i> Vehiculos</a></li>
+                            
+                        </ul>
+                    </li>
+                   
+                    <li class="treeview">
+                        <a href="#">
+                            <i class="fa fa-print"></i> <span>Reportes</span>
+                            <span class="pull-right-container">
+                                <i class="fa fa-angle-left pull-right"></i>
+                            </span>
+                        </a>
+                        <ul class="treeview-menu">
+
+    <li><a href="#"><i class="fa fa-circle-o"></i>Choferes</a></li>
+    <li><a href="#"><i class="fa fa-circle-o"></i>Vehiculos</a></li>
+                            
+                        </ul>
+                    </li>
+                   
+                </ul>
+            </section>
+            <!-- /.sidebar -->
+        </aside>
+
+        <!-- =============================================== -->
